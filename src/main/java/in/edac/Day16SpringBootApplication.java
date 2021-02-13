a75353e315fe6f1da4a0b8006e906489376fcdfe
Santosh Mondal <santosh.ece06@gmail.com>
2021-02-13 13:32:24 +0530

    b

--- src/main/java/in/edac/Day16SpringBootApplication.java
+++ src/main/java/in/edac/Day16SpringBootApplication.java
@@ -1,5 +1,7 @@
 package in.edac;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,8 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Day16SpringBootApplication implements CommandLineRunner {
 	
 	@Autowired
-	private DemoManager demoManager;
-
+	private UserRepository userRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Day16SpringBootApplication.class, args);
 	}
@@ -19,8 +21,58 @@ public class Day16SpringBootApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello Spring, this is run method");
-		System.out.println(demoManager);
+		
+		queryDemo();
 	}
-
+	
+	
+	public void queryDemo() {
+		
+		// List<User> list=  userRepository.findByUsername("mumbai");
+		// list.stream().map(User::getUsername).forEach(System.out::println);
+		
+		
+		// User user = userRepository.findByUsernameAndPassword("asdfa", "mumbai");
+		// System.out.println(user);
+		// System.out.println(user.getUsername());
+		
+		
+		List<User> list = userRepository.findByUsernameOrEmail("adfadfas", "mumbai@gmail.com");
+		list.stream().map(User::getUsername).forEach(System.out::println);
+	}
+	
+	
+	public void findAllDemo() {
+		List<User> list =  userRepository.findAll();
+		list.stream().map(User::getUsername).forEach(System.out::println);
+	}
+	
+	public void delete() {
+		userRepository.deleteById(1);
+	}
+	
+	
+	public void findAndUpdate() {
+		User user = userRepository.findById(2).get();
+		user.setUsername("MUMBAIIIIIIII");
+		
+		userRepository.save(user);
+	}
+	
+	
+	public void update() {
+		User user = new User();
+		user.setId(1);
+		user.setUsername("mumbaiii-delhi");
+		
+		
+		userRepository.save(user);
+	}
+	
+	
+	public void create() {
+		User user = new User("mumbai", "mumbai", "mumbai@gmail.com", "212121212");
+		userRepository.save(user);
+	}
 
 }
