93fffc0b18eaa78866f383b9cf22db5afc629b57
Santosh Mondal <santosh.ece06@gmail.com>
2021-02-13 12:10:43 +0530

    b1

--- src/main/java/in/edac/Day16SpringBootApplication.java
+++ src/main/java/in/edac/Day16SpringBootApplication.java
@@ -1,11 +1,15 @@
 package in.edac;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Day16SpringBootApplication implements CommandLineRunner {
+	
+	@Autowired
+	private DemoManager demoManager;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Day16SpringBootApplication.class, args);
@@ -14,10 +18,9 @@ public class Day16SpringBootApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		
 		System.out.println("Hello Spring, this is run method");
+		System.out.println(demoManager);
 	}
-
 
 
 }
---
+++ src/main/java/in/edac/DemoManager.java
@@ -0,0 +1,11 @@
+package in.edac;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoManager {
+
+	public void sayHi() {
+		System.out.println("I am demo manager");
+	}
+}
